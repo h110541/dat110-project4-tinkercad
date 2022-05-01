8d60b90bcec68f445a47b8be434639d37f53cbc2
h110541 <110541@stud.hvl.no>
2022-05-01 23:45:08 +0200

    ++

--- access_control_device.cc
+++ access_control_device.cc
@@ -2,12 +2,14 @@ void setup() {
     Serial.begin(9600);
 
     pinMode(2, INPUT); // motion sensor
-    pinMode(6, INPUT); // button2
     pinMode(7, INPUT); // button1
+    pinMode(6, INPUT); // button2
 
     pinMode(11, OUTPUT); // red LED
     pinMode(12, OUTPUT); // yellow LED
     pinMode(13, OUTPUT); // green LED
+
+    digitalWrite(11, HIGH);
 }
 
 
@@ -19,12 +21,21 @@ const int UNLOCKED = 4;
 
 int state = LOCKED;
 
+int push_handled1 = 0;
+int push_handled2 = 0;
+
 void loop() {
+    int motion_sensor = digitalRead(2);
     int button1 = digitalRead(7);
     int button2 = digitalRead(6);
 
     switch (state) {
         case LOCKED:
+            if (motion_sensor == HIGH) {
+                digitalWrite(11, LOW);
+                digitalWrite(12, HIGH);
+                state = WAITING1;
+            }
             break;
         case WAITING1:
             break;
